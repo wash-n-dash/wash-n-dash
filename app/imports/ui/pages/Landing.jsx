2786cb6c94ad70b255e9ef4b3b4614b580fd801d
jpham79 <35506835+jpham79@users.noreply.github.com>
2018-04-06 18:54:48 -1000

    Redone background
    Redid the background, Still need to fix logo.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable max-len */
 import React from 'react';
-import { Grid, Icon, Header } from 'semantic-ui-react';
+import { Image, Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
         <div className={'wash-landing-background'}>
+          <Image src='images/logo2.png' fluid />
           <Grid container stackable centered columns={3}>
 
             <Grid.Column textAlign='center'>
