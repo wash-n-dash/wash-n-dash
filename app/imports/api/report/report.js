721499bc6ac3fe239f1fa6cd5d3dc6b539570c04
jpham79 <35506835+jpham79@users.noreply.github.com>
2018-04-17 11:34:22 -1000

    Created report database Schema

---
+++ app/imports/api/report/report.js
@@ -0,0 +1,20 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Create a Meteor collection. */
+const Reports = new Mongo.Collection('Reports');
+
+/** Create a schema to constrain the structure of documents associated with this collection. */
+const ReportSchema = new SimpleSchema({
+  machineNumber: String,
+  reportedBy: String,
+  message: String,
+  createdAt: Date,
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Reports.attachSchema(ReportSchema);
+
+/** Make the collection and schema available to other code. */
+export { Reports, ReportSchema };
