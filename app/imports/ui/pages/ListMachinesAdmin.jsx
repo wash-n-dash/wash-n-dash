ee652a9b2697dd0e0fcbf51194b136cbb033afb5
David Badke <davidrbadke@gmail.com>
2018-04-23 23:55:19 -1000

    Finish implementing Admin page

--- app/imports/ui/pages/ListMachinesAdmin.jsx
+++ app/imports/ui/pages/ListMachinesAdmin.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Table, Container, Card, Header, Loader } from 'semantic-ui-react';
+import { Icon, Input, Dropdown, Checkbox, Button, Table, Container, Card, Header, Loader } from 'semantic-ui-react';
 import { Machines } from '/imports/api/machine/machine';
 import MachineAdmin from '/imports/ui/components/MachineAdmin';
 import { withTracker } from 'meteor/react-meteor-data';
@@ -18,23 +18,63 @@ class ListMachinesAdmin extends React.Component {
   renderPage() {
     return (
         <Container>
-          <Header as="h2" textAlign="center" inverted>List Machines</Header>
-          <Card.Group>
-            {this.props.machines.map((machine, index) => <MachineAdmin key={index} machine={machine}/>)}
-          </Card.Group>
+        <Header as="h2" textAlign="center" inverted>Machines Admin</Header>
         <Table celled>
           <Table.Header>
             <Table.HeaderCell>Machine</Table.HeaderCell>
+            <Table.HeaderCell>Type</Table.HeaderCell>
+            <Table.HeaderCell>Location</Table.HeaderCell>
             <Table.HeaderCell>Status</Table.HeaderCell>
             <Table.HeaderCell>Disable</Table.HeaderCell>
           </Table.Header>
-          <Table.body>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.body>
-        </Table>
-        </Container>
+          <Table.Body>
+              {this.props.machines.map((machine, index) =>
+                <Table.Row positive={machine.enabled === 'enabled'}
+                           negative={machine.enabled === 'disabled'}>
+                  <Table.Cell>{machine.machineNumber}</Table.Cell>
+                  <Table.Cell>
+                    <Dropdown
+                      options={[{text:'washer', value:'washer'}, {text: 'dryer', value: 'dryer'}]}
+                      placeholder='Choose an option'
+                      selection
+                      onChange={(e, d)=>Machines.update(
+                        { _id: machine._id },
+                        { $set: { machineType: d.value } })}
+                      value={machine.machineType}/>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Input 
+                      defaultValue={machine.location}
+                      onChange={(e, d)=>Machines.update(
+                        { _id: machine._id },
+                        { $set: { location: d.value } })} />
+                  </Table.Cell>
+                  <Table.Cell>{machine.timeRemaining} minutes remaining</Table.Cell>
+                  <Table.Cell>
+                    <Checkbox toggle
+                      defaultChecked={machine.enabled === 'enabled'}
+                      onChange={(e, d)=>Machines.update(
+                        { _id: machine._id },
+                        { $set: { enabled: d.checked ? 'enabled' : 'disabled' } })}
+                      label={machine.enabled}/>
+                  </Table.Cell>
+                </Table.Row>)}
+              </Table.Body>
+              <Table.Footer fullWidth>
+                <Table.Row>
+                  <Table.HeaderCell colSpan='5'>
+                    <Button floated='right' icon labelPosition='left' primary size='small'
+                      onClick={(e, d) => Machines.insert({
+                        enabled: 'disabled',
+                        machineNumber: this.props.machines.length + 1,
+                      })}>
+                      <Icon name='user' /> Add Machine
+                    </Button>
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Footer>
+            </Table>
+          </Container>
     );
   }
 }
@@ -48,7 +88,7 @@ ListMachinesAdmin.propTypes = {
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Machine documents.
-  const subscription = Meteor.subscribe('MachinesAdmin');
+  const subscription = Meteor.subscribe('Machines');
   return {
     machines: Machines.find({}).fetch(),
     ready: subscription.ready(),
